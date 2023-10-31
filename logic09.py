4ebdad528728352fd8614de1b05aa20f739bcc3e
elmurod <elmurodraimov764@gmail.com>
2023-10-31 19:23:12 +0500

    code

--- logic09.py
+++ logic09.py
@@ -1,10 +1,4 @@
 def main(a,b):
-    """
-    Given two integers a, b,  check the following statement "At least one of the numbers 'a' and 'b' is odd".
-    Args:
-        a(int): parameter a
-        b(int): parameter b
-    Returns:
-        bool: answer
-    """
-    return
+    return a%2==0 or b%2==1
+print(main(6,3))
+print(main(7,1))
