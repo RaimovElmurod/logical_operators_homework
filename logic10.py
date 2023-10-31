6c1330ae55493ef1d8cf8533de3e0e9a56f185e9
elmurod <elmurodraimov764@gmail.com>
2023-10-31 19:35:57 +0500

    code

--- logic10.py
+++ logic10.py
@@ -1,9 +1,4 @@
 def main(a):
-    """
-    Given integer a,  check the following statement "The integer is two-digit number".
-    Args:
-        a(int): parameter a
-    Returns:
-        bool: answer
-    """
-    return
+    return bool(a//10!=0)
+print(main(3))
+print(main(12))
