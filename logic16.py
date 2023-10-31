89e4ccb8dbccfa41243317ad98eb192aaa0e2d48
elmurod <elmurodraimov764@gmail.com>
2023-10-31 20:12:37 +0500

    code

--- logic16.py
+++ logic16.py
@@ -1,9 +1,4 @@
 def main(a):
-    """
-    Given integer a,  check the following statement "The integer is a five-digit number".
-    Args:
-        a(int): parameter a
-    Returns:
-        bool: answer
-    """
-    return
+    return bool(a//10000!=0)
+print(main(15234))
+print(main(763))
