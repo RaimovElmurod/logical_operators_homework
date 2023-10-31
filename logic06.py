c6cb213d6cfd04106aaf1444d8a72f5e42fc8280
elmurod <elmurodraimov764@gmail.com>
2023-10-31 19:14:11 +0500

    code

--- logic06.py
+++ logic06.py
@@ -1,10 +1,4 @@
 def main(a,b):
-    """
-    Given two integers a, b,  check the following statement "At least one of the numbers 'a' and 'b' is positive".
-    Args:
-        a(int): parameter a
-        b(int): parameter b
-    Returns:
-        bool: answer
-    """
-    return
+    return a>0 or b>0
+print(main(-3,8))
+print(main(-9,-1))
