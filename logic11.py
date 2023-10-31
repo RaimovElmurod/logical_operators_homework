dd8013bc94f6c132de7283b61ad25ec0229f2afa
elmurod <elmurodraimov764@gmail.com>
2023-10-31 19:46:46 +0500

    code

--- logic11.py
+++ logic11.py
@@ -1,9 +1,5 @@
 def main(a):
-    """
-    Given integer a,  check the following statement "The integer is three-digit number".
-    Args:
-        a(int): parameter a
-    Returns:
-        bool: answer
-    """
-    return
+    return bool(a//100!=0)
+print(main(3))
+print(main(12))
+print(main(123))
