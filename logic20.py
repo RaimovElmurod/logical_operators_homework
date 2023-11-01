a086596280f03f2595da2879be517eeb9666593e
elmurod <elmurodraimov764@gmail.com>
2023-11-01 20:31:45 +0500

    code

--- logic20.py
+++ logic20.py
@@ -1,12 +1,15 @@
 def main(n):
-    """
-    A number consisting of one and zero is given (the number starts at once). 
-    If the number of ones is greater than zero, true, otherwise False is returned.
-    
-    Args:
-        n(int): parameter n
-    Returns:
-        bool: answer
-    """
-
-    return
+    x1 = n%10
+    n//=10
+    x2 = n%10
+    n//=10
+    x3 = n%10
+    n//=10
+    x4 = n%10
+    n//=10
+    x5 = n%10
+    ones = (x1+ x2 + x3 + x4 + x5)
+    zeros = 5 - ones
+    return ones> zeros
+print(main(1100))
+print(main(10011))
