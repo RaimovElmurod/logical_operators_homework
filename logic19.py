f446a5d05c3e1372b02effb3cd9bb4edff230818
elmurod <elmurodraimov764@gmail.com>
2023-11-01 20:32:46 +0500

    code

--- logic19.py
+++ logic19.py
@@ -1,11 +1,6 @@
+import math
 def main(x):
-    """
-    Given a two digit integer x, return true if x is palindrome integer.
-    An integer is a palindrome, when it reads the same backward as forward.
-
-    Args:
-        x(int): parameter x
-    Returns:
-        bool: answer
-    """
-    return 
+    return math.sqrt(x)%1==0
+print(main(121))
+print(main(10))
+print(main(11))
